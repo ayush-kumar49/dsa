a511a3e54ef0cbc5d9a68f17945abaedc47f414f
Ayush Kumar <ayush.kumar49@outlook.com>
2025-10-05 13:52:27 +0530

    Initial Commit

---
+++ cpp_basics/userInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter a number: ";
+    int x;
+    cin >> x;
+    cout << "The square of this nuber is :" << x*x;
+    return 0;
+}
